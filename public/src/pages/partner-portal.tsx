e202225516c103aed0b497539c63701acebbf6d6
bbowden@gmail.com <bbowden@gmail.com>
2025-09-26 16:00:12 -0400

    Add partner signup link below partner ID field

--- public/src/pages/partner-portal.tsx
+++ public/src/pages/partner-portal.tsx
@@ -345,6 +345,20 @@ export default function PartnerPortal() {
             </button>
           </div>
           
+          <div className="mt-4 text-center">
+            <p className="text-sm text-gray-600">
+              Want to be a partner?{" "}
+              <a 
+                href="https://fullyfurnishedministries.org/partners" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:text-blue-800 underline"
+              >
+                click here
+              </a>
+            </p>
+          </div>
+          
           {error && (
             <div className="mt-4 flex items-center text-red-600 bg-red-50 border border-red-200 rounded-lg p-4">
               <AlertCircle className="w-5 h-5 mr-3" />
